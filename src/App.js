268856e7bdcbc26a5386d95784efc1f06bebc9e2
Burt Wang <burtwang@universalrail.com>
2018-07-19 09:16:43 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo"/>
+          <h1 className="App-title">Welcome to React: </h1>
+          <h2>Project #4, component life cycle</h2>
+        </header>
+        <Body/>
+      </div>
+    );
+  }
+}
+
+class Body extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      randomNumber: 10
+    };
+    this.getRandomNumber = this.getRandomNumber.bind(this);
+  }
+
+  render() {
+    return (
+      <div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <button onClick={this.getRandomNumber}>Random Number</button>
+        <RandomNumbers myNumber={this.state.randomNumber}/>
+      </div>
+    );
+  }
+
+  getRandomNumber() {
+    this.setState({randomNumber: Math.floor(Math.random() * 10)});
+
+    console.log("Random number called ");
+  }
+}
+
+class RandomNumbers extends Component {
+  render() {
+    return (
+      <div>
+        <br/>
+        {this.props.myNumber}
+      </div>
+    );
+  }
+}
+
+
+export default App;
